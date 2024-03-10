ed242366dd9532ba8b5f75ae6483f28307169bf3
smithe84 <smithe84@wwu.edu>
2024-03-10 14:14:08 -0700

    Added descriptive comments.

--- config/boards/arm/mmkb/board.cmake
+++ config/boards/arm/mmkb/board.cmake
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 #
 
+# cmake directives on how to flash microcontroller
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)

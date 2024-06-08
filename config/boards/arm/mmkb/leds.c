733e2cff821c17c58cb3dabdd9486261e0733cc5
smithe84 <smithe84@wwu.edu>
2024-06-07 19:25:05 -0700

    added "()"

--- config/boards/arm/mmkb/leds.c
+++ config/boards/arm/mmkb/leds.c
@@ -28,7 +28,7 @@ static int led_capslock_listener_cb(const zmk_event_t *eh) {
     zmk_hid_indicators_t flags = zmk_hid_indicators_get_current_profile();
     LOG_INF("current flags: %d", flags);
 
-    if (flags & 0x10) {
+    if (flags & (0x10)) {
         LOG_INF("CAPSLOCK is on");
         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_main)));
 
